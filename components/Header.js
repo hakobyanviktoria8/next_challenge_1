1617aca229798afa9e1c0a3d5254d15b0086a738
hakobyanviktoria8 <hakobyanviktorya8@gmail.com>
2022-04-07 01:37:44 +0400

    add tailwind for style

--- components/Header.js
+++ components/Header.js
@@ -1,8 +1,23 @@
 import React from 'react'
+import Link from 'next/link'
 
 function Header() {
   return (
-    <div>Header</div>
+    <div>
+        <h1 className="text-3xl font-bold underline">
+         Hello world!
+        </h1>
+        <div>
+            <Link href="/">LOGO</Link>
+            <Link href="/about">About</Link>
+            <Link href="/contact">Contact</Link>
+            <Link href="/follow">Follow</Link>
+        </div>
+        <div>
+        <Link href="/sign-in">Sign in</Link>
+        <Link href="/get-started">Get started</Link>
+        </div>
+    </div>
   )
 }
 
